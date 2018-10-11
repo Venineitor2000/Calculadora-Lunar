6d83d9e2135f69ab62c45311ac3ac8295d56836d
Juanma <altunajuan@gmail.com>
2018-10-11 20:55:19 -0300

    Holuuu

---
+++ Cumple/Cumple.js
@@ -0,0 +1,26 @@
+var Cumple_HTML =
+
+"  <p>"
+  "Bueno bueno bueno, miren quien se me une al club de los ancianitos, nada menos que la mejor chiquita labio del mundo 😂 "
+  +
+  "<br><br> Primero que nada te quiero decir, muy feliz cumple locucha mia y espero que la pases de 10 en este hermoso dia!"
+  +
+  "<br><br>No se si este con vos cuando leas esto o no, pero ojala que si por que si te soy sincero me hubiera encantado poder pasar cumples juntos con vos desde hace años y aun que por motivos que nunca voy a entender del todo no nos hablabamos y obvio no lo haciamos por tanto. Ahora quisiera aprovechar cada cumpleaños al maximo! Y en especial uno tan importante como este, despues de todo no todos los años se cumple 18 🤗 "
+  +
+  "<br><br>Y aun que seguro ya lo sepas, quiero decirte que para mi sos una persona asombrosa y sin igual, desde tu gran corazon que puedo asegurar por experiencia propia que es inmenso como el de pocos en especial en una sociedad como la de estos dias, hasta cosas como tu personalidad arisca pero que cuando queres, podes ser mas dulce que nadie, o tu belleza de angel tanto externa como interna. ❤️"
+  +
+  "<br><br> A decir verdad me encanta hablar con vos y saber que siempre voy a poder contar con tu ayuda cuando lo necesite, y por supuesto vos no dudes jamas de lo mismo con la mia 😉"
+  +
+  "<br><br> Asi que muchas gracias por ser mi mejor amiga!!! La verdad que mi vida no seria lo mismo sin vos en ningun sentido, me ayudas a crecer en multiples sentidos dia a dia, y eso es algo que siempre te voy a agradecer ❤️"
+  +
+  "<br><br>Por muchisimos mas cumple juntos! Y que nunca nada rompa nuestra amistad capaz de enfrentarse a todo!"
+  +
+  "<br><br> MUY FELIZ LEGALIDAD MARTU!!! TE QUIERO HASTA EL INFINITO Y MAS ALLA!!! 🔞💞💗"
+  +
+  "<br><br>🐀 + 👄 = 💕 "
+  +
+"  </p>"
+
+
+
+var Cumple_CSS =

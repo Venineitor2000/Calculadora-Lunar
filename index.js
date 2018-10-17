96dc597f8a6a267ac2984bffd490d1536c10360b
Juanma <altunajuan@gmail.com>
2018-10-16 21:14:00 -0300

    Holi1111111310014030230400099110961101

--- index.js
+++ index.js
@@ -163,12 +163,10 @@ if(dias_faltantes == 0)
 {
   if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i)|| device.match(/CriOS/i))
    {
-     alert("Hola")
      location.href = paginas[fecha_proxima] + "_Celu.html";
    }
    else
    {
-     alert("Chau")
      location.href = paginas[fecha_proxima] + "_Compu.html";
    }
 }
